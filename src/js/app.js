35be8313a5a54bdc2bbff5f02b4a5db0478cfe82
unknown <delight_1@mail.ru>
2022-12-04 10:22:08 +0500

    Комит 22

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,33 @@ import * as flsFunctions from "./modules/functions.js"
 
 flsFunctions.isWebP()
 
-import Swiper, { Navigation, Pagination } from 'swiper';
 
-const swiper = new Swiper()
+
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+
+
+const swiper = new Swiper('.swiper', {
+    effect: "fade",
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.custom-next',
+      prevEl: '.custom-prev'
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+  
+    
